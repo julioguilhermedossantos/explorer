c82b837a1658b229b99057371577451f4cf009dc
Júlio Guilherme <julio.guilherme@b2wdigital.com>
2022-08-11 20:04:58 -0300

    enabling swagger doc

---
+++ src/main/java/br/com/elo7/explorer/config/swagger/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package br.com.elo7.explorer.config.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI contentaAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Desafio elo7 - Sondas e planetas")
+                        .description("API explorer")
+                        .version("v1"));
+    }
+}
+
