ed5904d073cc9db0bdb0cf7891d5eb5c3d7c03c8
Júlio Guilherme <julio.guilherme@b2wdigital.com>
2022-08-12 18:41:58 -0300

    enhancing enum PointTo. Strategy to change orientation

--- src/main/java/br/com/elo7/explorer/probe/enums/PointTo.java
+++ src/main/java/br/com/elo7/explorer/probe/enums/PointTo.java
@@ -1,5 +1,39 @@
 package br.com.elo7.explorer.probe.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum PointTo {
-    NORTH, SOUTH, EAST, WEST;
+    NORTH(1) {
+        @Override
+        public PointTo changeOrientation(Character direction) {
+            return direction.equals(AllowedActions.L.getValue()) ? WEST : EAST;
+        }
+
+    },
+    SOUTH(-1) {
+        @Override
+        public PointTo changeOrientation(Character direction) {
+            return direction.equals(AllowedActions.L.getValue()) ? EAST : WEST;
+        }
+    },
+    WEST(-1) {
+        @Override
+        public PointTo changeOrientation(Character direction) {
+            return direction.equals(AllowedActions.L.getValue()) ? SOUTH : NORTH;
+        }
+    },
+    EAST(1) {
+        @Override
+        public PointTo changeOrientation(Character direction) {
+            return direction.equals(AllowedActions.L.getValue()) ? NORTH : SOUTH;
+        }
+    };
+
+    private Integer step;
+
+    public abstract PointTo changeOrientation(Character direction);
+
 }
