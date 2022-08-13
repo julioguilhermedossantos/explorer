bbed03e96012451e54dba7524492925f0fc65431
Júlio Guilherme <julio.guilherme@b2wdigital.com>
2022-08-13 13:02:38 -0300

    addind valid turn actions enum

---
+++ src/main/java/br/com/elo7/explorer/probe/enums/TurnActions.java
@@ -0,0 +1,12 @@
+package br.com.elo7.explorer.probe.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum TurnActions {
+    L('L'), R('R');
+
+    private final char value;
+}
