0c7caf0a6c76c1c9116663e08c9b17465e0a6b7c
Júlio Guilherme <julio.guilherme@b2wdigital.com>
2022-08-13 14:07:48 -0300

    removing unused method

--- src/main/java/br/com/elo7/explorer/planet/repository/PlanetRepository.java
+++ src/main/java/br/com/elo7/explorer/planet/repository/PlanetRepository.java
@@ -4,11 +4,6 @@ import br.com.elo7.explorer.planet.model.Planet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface PlanetRepository extends JpaRepository<Planet, Long> {
-
-    public Optional<Planet> findByName(String planetName);
-
 }
