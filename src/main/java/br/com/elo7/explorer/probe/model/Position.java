316103891152eb7f96c546e1ea43ed6eb377465e
Júlio Guilherme <julio.guilherme@b2wdigital.com>
2022-08-13 10:07:09 -0300

    move probe first implementation

--- src/main/java/br/com/elo7/explorer/probe/model/Position.java
+++ src/main/java/br/com/elo7/explorer/probe/model/Position.java
@@ -16,4 +16,11 @@ public class Position {
     private Integer coordinateX;
     private Integer coordinateY;
 
+    public void verticalMove (Integer amount){
+        coordinateY += amount;
+    }
+
+    public void horizontalMove (Integer amount){
+        coordinateX += amount;
+    }
 }
--- src/main/java/br/com/elo7/explorer/probe/model/Probe.java
+++ src/main/java/br/com/elo7/explorer/probe/model/Probe.java
@@ -1,10 +1,12 @@
 package br.com.elo7.explorer.probe.model;
 
 import br.com.elo7.explorer.planet.model.Planet;
+import br.com.elo7.explorer.probe.enums.PointTo;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.stream.Stream;
 
 @Entity
 @Getter
@@ -24,10 +26,30 @@ public class Probe {
     @Embedded
     private Position position;
 
-    private String pointingTo;
+    @Enumerated(EnumType.STRING)
+    private PointTo pointingTo;
 
     @JsonBackReference
     @JoinColumn(name = "planet_id")
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne
     private Planet currentExlporingPlanet;
+
+    public void execute(Character action) {
+        var turnActions = new char[]{'L', 'R'};
+
+        if (Stream.of(turnActions).anyMatch(action::equals)) {
+            pointingTo.changeOrientation(action);
+        }else {
+            move();
+        }
+    }
+
+    private void move() {
+        var verticalMove = new PointTo[]{PointTo.NORTH, PointTo.SOUTH};
+        if (Stream.of(verticalMove).anyMatch(pointTo -> pointTo == pointingTo)) {
+            position.verticalMove(pointingTo.getStep());
+        } else {
+            position.horizontalMove(pointingTo.getStep());
+        }
+    }
 }
